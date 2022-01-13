d2c83c8c3c4855cf5601c71366fd036a6b6cf876
Julian <portalsoup@gmail.com>
2022-01-13 13:37:26 -0800

    didnt' quote the arg

--- platform/general/source/git.sh
+++ platform/general/source/git.sh
@@ -32,6 +32,6 @@ gclone() {
     if [ "$2" ]; then
         git clone git@github.com:$1/$2.git
     elif [ "$1" ]; then
-        git clone git@github.com:$(gctx))/$1.git
+        git clone git@github.com:$(gctx))/"$1".git
     fi
 }
