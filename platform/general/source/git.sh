ef093bcc8eaf4b6f238bca3f82cb542c393cab21
Julian <portalsoup@gmail.com>
2022-01-13 13:38:49 -0800

    didnt' quote more args

--- platform/general/source/git.sh
+++ platform/general/source/git.sh
@@ -30,8 +30,8 @@ gh() {
 # Clones a repo to the current
 gclone() {
     if [ "$2" ]; then
-        git clone git@github.com:$1/$2.git
+        git clone "git@github.com:$1/$2.git"
     elif [ "$1" ]; then
-        git clone git@github.com:$(gctx))/"$1".git
+        git clone "git@github.com:$(gctx))/$1.git"
     fi
 }
