398bc287db0c276d557b5b6b2f6df7a115b4891e
Julian <portalsoup@gmail.com>
2022-01-13 13:47:53 -0800

    strip quotes from jq output

--- platform/general/source/git.sh
+++ platform/general/source/git.sh
@@ -5,7 +5,7 @@
 # Uses the file at ~/.zsh_scripts/config.json to persist configuration
 gctx() {
     if [ -z "$1" ]; then
-        jq ".git_context" ~/.zsh_scripts/config.json
+        jq -r ".git_context" ~/.zsh_scripts/config.json
         return 0
     fi 
     jq --arg value "$1" '.git_context = $value' ~/.zsh_scripts/config.json | sponge ~/.zsh_scripts/config.json  
@@ -32,6 +32,6 @@ gclone() {
     if [ "$2" ]; then
         git clone "git@github.com:$1/$2.git"
     elif [ "$1" ]; then
-        git clone "git@github.com:$(gctx))/$1.git"
+        git clone "git@github.com:$(gctx)/$1.git"
     fi
 }
